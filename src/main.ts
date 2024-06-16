ce21e63b99bdd6a8bbd6ca2c2b42d6ebf0eefe16
Vitor Oliveira. <caulicons@gmail.com>
2024-06-16 20:06:21 -0300

    feat: add swagger support

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Taxi - Backend')
+    .setDescription('Uber clone API.')
+    .setVersion('0.000000001')
+    .addTag('Uber')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(4000);
 }
 bootstrap();
